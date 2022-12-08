df8a57c7dd579fa461eddc442ca2a0af9f4c0d49
balesic <balesic@student.chalmers.se>
2022-12-08 13:51:22 +0100

    Refactor 'createClinic' method

--- src/models/Clinic.ts
+++ src/models/Clinic.ts
@@ -6,18 +6,19 @@ const clinicSchema = new mongoose.Schema({
   name: { type: String, required: true },
   owner: { type: String, required: true },
   dentists: { type: Number, required: true },
-  adress: { type: String, required: true },
-  coordinates: [{ 
+  address: { type: String, required: true },
+  city: { type: String, required: true},
+  coordinate: { 
     longitude: { type: String, required: true }, 
     latitude: { type: String, required: true } 
-  }],
-  openingHours: [{ 
-    Monday: { type: String, required: true },
-    Tuesday: {type: String, required: true },
-    Wednesday: {type: String, required: true},
-    Thursday: {type: String, required: true},
-    Friday: {type: String, required: true}
-  }]
+  },
+  openinghours: { 
+    monday: { type: String, required: true },
+    tuesday: {type: String, required: true },
+    wednesday: {type: String, required: true},
+    thursday: {type: String, required: true},
+    friday: {type: String, required: true}
+  }
 })
   
 // Export Clinic
