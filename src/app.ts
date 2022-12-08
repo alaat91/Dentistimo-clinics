4f61e4a11401d07a25efa77c36808e26b5567b67
balesic <balesic@student.chalmers.se>
2022-12-08 12:02:08 +0100

    Implement 'getClinic' method

--- src/app.ts
+++ src/app.ts
@@ -27,6 +27,7 @@ mongoose.connect(
 
 client.on('connect', () => {
   client.subscribe('clinic/post')
+  client.subscribe('clinic/get')
 })
 
 client.on('message', async(topic: string, message: Buffer) => {
@@ -34,13 +35,15 @@ client.on('message', async(topic: string, message: Buffer) => {
     case 'clinic/post': {
       // call createUser function
       // eslint-disable-next-line no-console
-      console.log('kladdkaka2')
       const newClinic = await clinic.createClinic(message.toString())
       client.publish('gateway/clinic/post', JSON.stringify(newClinic))
       break
     }
     case 'clinic/get': {
       // call getClinic function
+      // eslint-disable-next-line no-console
+      const existingClinic = await clinic.getClinic(message.toString())
+      client.publish('gateway/clinic/get', JSON.stringify(existingClinic))
       break
     }
     case 'clinic/getall':
